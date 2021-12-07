8912b1a30c5f2709f6c81c9ece0816aa5786ecac
Jonah Pine <jpine4@hotmail.com>
2021-12-07 18:53:25 -0500

    Implemented basic views and serializers

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
+# @admin.register(models.Post)
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ("title", 'id', 'status', 'slug', 'author')
+#     prepopulated_fields = {'slug' : ('title',), }
 
 # Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Post)
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db.models import query
+from django.db.models.query import QuerySet
+from rest_framework import generics
+from blog.models import *
+from .serializers import *
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postObjects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
